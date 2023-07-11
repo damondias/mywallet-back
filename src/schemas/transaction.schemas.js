b75047e1021e8b464b6bd7f62016a17b75c9dd18
Damon <damondias@gmail.com>
2023-07-11 15:08:06 -0400

    fix: Amount positive incorrect

--- src/schemas/transaction.schemas.js
+++ src/schemas/transaction.schemas.js
@@ -1,7 +1,7 @@
 import joi from 'joi';
 
 const transactionSchema = joi.object({
-  amount: joi.number().required(),
+  amount: joi.number().positive().required(),
   description: joi.string().required(),
   type: joi.string().valid("entrada", "saida")
 });
